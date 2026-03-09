e6429da8e0b6d5762397b37950b20d9d8aa6c3f3
Haneen Amr <haneen.amr.1210@gmail.com>
2026-03-09 19:35:37 +0200

    removed unnecessary function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { viewProfile, updateUser } = require("../controllers/userController");
 
-const {
-  checkAuth,
-  protect,
-  restrictTo,
-  isOwner,
-} = require("../middlewares/AuthMW");
+const { protect, restrictTo, isOwner } = require("../middlewares/AuthMW");
 const validateMW = require("../middlewares/schemaValidatorMW");
 const checkUniqueness = require("../middlewares/userValidatorMW");
 const { updateValidator } = require("../utils/validators/userValidator");
